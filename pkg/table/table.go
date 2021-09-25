9956508b863b6e3bfa159f93f8777f142bafb3f2
swarupdonepudi <swarup.donepudi@gmail.com>
2021-09-25 06:30:56 -0700

    added support for table with sort and col configs

--- pkg/table/table.go
+++ pkg/table/table.go
@@ -7,6 +7,45 @@ import (
 
 func PrintTable(header table.Row, rows []table.Row) {
 	println("")
+	t := getDefaultTableWriter(header, rows)
+	t.Render()
+	println("")
+}
+
+func PrintTableWithSort(header table.Row, rows []table.Row, sortRules ...table.SortBy) {
+	println("")
+	t := getDefaultTableWriter(header, rows)
+	if sortRules != nil && len(sortRules) > 0 {
+		t.SortBy(sortRules)
+	}
+	t.Render()
+	println("")
+}
+
+func PrintTableWithColConfig(header table.Row, rows []table.Row, colConfigs ...table.ColumnConfig) {
+	println("")
+	t := getDefaultTableWriter(header, rows)
+	if colConfigs != nil && len(colConfigs) > 0 {
+		t.SetColumnConfigs(colConfigs)
+	}
+	t.Render()
+	println("")
+}
+
+func PrintTableWithSortAndColConfig(header table.Row, rows []table.Row, sortRules []table.SortBy, colConfigs []table.ColumnConfig) {
+	println("")
+	t := getDefaultTableWriter(header, rows)
+	if sortRules != nil && len(sortRules) > 0 {
+		t.SortBy(sortRules)
+	}
+	if colConfigs != nil && len(colConfigs) > 0 {
+		t.SetColumnConfigs(colConfigs)
+	}
+	t.Render()
+	println("")
+}
+
+func getDefaultTableWriter(header table.Row, rows []table.Row) table.Writer {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	if header != nil {
@@ -16,6 +55,5 @@ func PrintTable(header table.Row, rows []table.Row) {
 		t.AppendRow(r)
 		t.AppendSeparator()
 	}
-	t.Render()
-	println("")
+	return t
 }
