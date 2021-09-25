f696c91f4ba9062696f8f0b74c95e3f87c6fb863
swarupdonepudi <swarup.donepudi@gmail.com>
2021-09-25 10:43:06 -0700

    slice to multi line sltring

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -1,5 +1,7 @@
 package slice
 
+import "strings"
+
 func ContainsString(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
@@ -7,4 +9,16 @@ func ContainsString(s []string, str string) bool {
 		}
 	}
 	return false
+}
+
+func ToMultiLineString(s []string) string {
+	var b strings.Builder
+	for i, e := range s {
+		b.WriteString(e)
+		if i == len(s)-1 {
+			break
+		}
+		b.WriteString("\n")
+	}
+	return b.String()
 }
